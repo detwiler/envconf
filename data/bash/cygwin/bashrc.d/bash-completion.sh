a422cc015a33395c8bb2547af1564619207793c0
Mike Detwiler <mike.detwiler@gmail.com>
2010-01-04 07:25:09 -0500

    cygwin specific configuration

--- data/bash/cygwin/bashrc.d/bash-completion.sh
+++ data/bash/cygwin/bashrc.d/bash-completion.sh
@@ -6,12 +6,8 @@
 # If this shell is interactive, turn on programmable completion enhancements.
 # Any completions you add in ~/.bash_completion are sourced last.
 
-# we don't need to do this for Fedora as of release 8
-if [[ $(uname --operating-system) == "Cygwin" ]]; then
+case $- in
 
-  case $- in
+  *i*) [[ -f /etc/bash_completion ]] && . /etc/bash_completion ;;
 
-    *i*) [[ -f /etc/bash_completion ]] && . /etc/bash_completion ;;
-
-  esac
-fi
+esac
