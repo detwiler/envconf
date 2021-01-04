d0b65ec84a0feb6cb55a2eafe97db21e17b3c721
Mike Detwiler <mike@detwiler.io>
2021-01-04 06:49:35 -0500

    Issue #94: Cleanup
    Signed-off-by: Mike Detwiler <mike@detwiler.io>

--- rules.mk
+++ rules.mk
@@ -1,5 +1,7 @@
 # rules.mk
 
 # generate an output file from an input template via sed
+# define EDIT with an appropriate sed expression in the Makefile.am responsible
+# for building the output file
 %: %.in
 	$(AM_V_GEN)$(SED) $(EDIT) < $< > $@
